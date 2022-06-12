506e101eb7ac19f374665a7f56841dfbc254ae86
Segiladee <segilade0611@gmail.com>
2022-06-12 06:25:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet ten times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
