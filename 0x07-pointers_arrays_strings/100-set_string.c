30da6cbca7cc5e585ef4d45f231742b13395c33b
Segiladee <segilade0611@gmail.com>
2022-06-22 02:39:04 -0700

    string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * set_string - sets pointer val to char
- * @s: pointer
- * @to: char
+ * set_string - sets pointer to char
+ * @s : pointer
+ * @to : char
  */
 
 void set_string(char **s, char *to)
