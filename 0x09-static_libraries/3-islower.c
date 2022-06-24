ad4aeed34309847e310929cf8bf3897f49c711f9
Segiladee <segilade0611@gmail.com>
2022-06-24 06:13:56 -0700

    Library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks if c is in lowercase and returns 1 if not returns 0
+ * @c: character argument
+ * Return: 1 and 0 depending on condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
